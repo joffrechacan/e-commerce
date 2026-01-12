81bc3ee9b612d146cf14d42a782dc141c41b551b
Joffre Chacan <Joffre Chacan>
2026-01-12 11:53:49 -0500

    Agregar ejs al proyecto

--- app.js
+++ app.js
@@ -7,6 +7,13 @@ const app = express();
 //3. Definir un puerto sobre el cual funciona nuestra app
 const PORT = 3000;
 
+// ---CONFIGURACION---
+// Establecer EJS como mortor de vistas
+app.set('view engine', 'ejs');
+
+// Establecer la carpeta publica con los elementos estaticos 
+app.use(express.estatic('public'));
+
 //4. Crear la primera "ruta" cuando el usuario ingrese a la raiz de nuestra app 
 app.get ('/', (req, res) =>{
     res.send('<h1>Servidor de E-commerce Iniciado </h1><p>NPM y Express funcionando...!<p/>')
