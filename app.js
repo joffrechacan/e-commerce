5684efd0b5cd1a8a32f249318e2fcb1ac7a6ef02
Joffre Chacan <Joffre Chacan>
2026-01-16 10:11:40 -0500

    categorias y add README

--- app.js
+++ app.js
@@ -1,26 +1,79 @@
-//1. importar la libreria para crear aplicaciones web 
-const express = require ('express');
+// 1. Importar la librería para crear aplicaciones Web
+const express = require('express');
 
-//2. Crear una instancia de express (aplicacion principal)
+// 2. Crear una instacia de express (aplicación principal)
 const app = express();
 
-//3. Definir un puerto sobre el cual funciona nuestra app
+// 3. Definir un puerto sobre el cual funciona nuestra app
 const PORT = 3000;
 
-// ---CONFIGURACION---
-// Establecer EJS como mortor de vistas
+// --- CONFIGURACIÓN ---
+// Establecer EJS como motor de vistas
 app.set('view engine', 'ejs');
 
-// Establecer la carpeta publica con los elementos estaticos 
-app.use(express.estatic('public'));
+// Establecer la carpeta publica con elementos estáticos
+app.use(express.static('public'));
 
-//4. Crear la primera "ruta" cuando el usuario ingrese a la raiz de nuestra app 
-app.get ('/', (req, res) =>{
-    res.send('<h1>Servidor de E-commerce Iniciado </h1><p>NPM y Express funcionando...!<p/>')
-})
+// Simular una lista de productos
+const listaProductos = [
+    {
+    nombre: "Laptop Pro",
+    precio: 850,
+    categoria: "computadores",
+    imagen: "https://dummyimage.com/200x200/000/fff&text=Laptop"
+},
+{
+    nombre: "PC Desktop Gamer",
+    precio: 1200,
+    categoria: "computadores",
+    imagen: "https://dummyimage.com/200x200/000/fff&text=PC+Gamer"
+},
+{
+    nombre: "Audifonos Sony",
+    precio: 125,
+    categoria: "perifericos",
+    imagen: "https://dummyimage.com/200x200/000/fff&text=Audifonos"
+},
+{
+    nombre: "Mouse ergonómico",
+    precio: 50,
+    categoria: "perifericos",
+    imagen: "https://dummyimage.com/200x200/000/fff&text=Mouse"
+},
+{
+    nombre: "Teclado Mecánico",
+    precio: 80,
+    categoria: "perifericos",
+    imagen: "https://dummyimage.com/200x200/000/fff&text=Teclado"
+}
+];
 
-//5. Encender el servidor 
-app.listen(PORT, ()=> {
-    console.log (`>>> Servidor corriendo en http://localhost:${PORT}`);
+
+// -- RUTAS --
+app.get('/', (req, res) => {
+    // Renderizar la plantilla con los datos proporcionados
+    res.render('index', { 
+        productos: listaProductos, 
+        titulo: "Todos los Productos" 
+    }); 
+});
+
+// Ruta dinámica para categorías
+app.get('/categoria/:nombreCategoria', (req, res) => {
+    const cat = req.params.nombreCategoria;
+    
+    // Filtramos el arreglo según la categoría de la URL
+    const productosFiltrados = listaProductos.filter(
+        p => p.categoria === cat);
+    
+    res.render('index', { 
+        productos: productosFiltrados, 
+        titulo: cat.charAt(0).toUpperCase() + cat.slice(1) // Para poner la primera letra en mayúscula
+    });
+});
+
+// 5. Encender el servidor
+app.listen(PORT, () =>{
+    console.log(`>>> Servidor corriendo en http://localhost:${PORT}`);
     console.log(`>>> Presione Ctrl + c para detener`);
 });
